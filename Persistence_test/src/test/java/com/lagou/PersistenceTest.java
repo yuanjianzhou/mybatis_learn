48140e996815540ca703d1032f14c5db994e8627
jason <jason@gotobus.com>
2020-04-23 17:09:04 +0800

    rename test

--- Persistence_test/src/test/java/com/lagou/PersistenceTest.java
+++ Persistence_test/src/test/java/com/lagou/PersistenceTest.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 import java.io.InputStream;
 import java.util.List;
 
-public class IPrenceTest {
+public class PersistenceTest {
 
     @Test
     public void test() throws Exception {
